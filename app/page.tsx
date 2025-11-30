e9d51aa458e2d06f9fab9ede211dd5e5cec922ea
Stian <stianoyejenssen@gmail.com>
2025-11-30 12:18:55 +0100

    feat: Add internationalization

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex flex-col w-full items-center gap-4 h-min text-center">
-      <h1 className="text-4xl">Big Things Comming!</h1>
-      <p className="italic">Stay tuned</p>
-    </main>
-  );
-}
