f99a3b8874c809dee1a98107223ddad2b5104bd4
Stian <stianoyejenssen@gmail.com>
2026-01-24 19:40:08 +0100

    fix: Tweak title

--- app/page.tsx
+++ app/page.tsx
@@ -5,39 +5,68 @@ export default function Home() {
     <div>
       <div className="flex min-h-screen flex-col items-center justify-center pt-4 bg-[linear-gradient(var(--accent)_1px,transparent_1px),linear-gradient(90deg,var(--accent)_1px,transparent_1px)] bg-[size:40px_40px] bg-[position:20px_20px] bg-fixed">
         <main className="flex flex-col w-full items-center gap-4 h-min text-center">
-          <h1 className="text-4xl">Big Things Comming!</h1>
+          <h1 className="text-4xl">Big Things Coming!</h1>
           <p className="italic">Stay tuned</p>
         </main>
       </div>
-      <div className="w-screen flex flex-col items-center justify-center p-4 gap-4">
+      {/* <div className="w-full flex flex-col items-center justify-center pt-4 pb-4 gap-4">
         <h2 className="text-2xl">Projects</h2>
-        <div className="flex gap-4">
+        <div className="flex flex-col gap-4">
           <Card>
             <CardHeader>
-              <CardTitle>This is a card</CardTitle>
+              <CardTitle>Portfolio</CardTitle>
             </CardHeader>
             <CardContent>
-              <p>This is some content inside this wonderfull card</p>
+              <p>Website where i show off some of my work</p>
             </CardContent>
           </Card>
           <Card>
             <CardHeader>
-              <CardTitle>This is a card</CardTitle>
+              <CardTitle>Driveo</CardTitle>
             </CardHeader>
             <CardContent>
-              <p>This is some content inside this wonderfull card</p>
+              <p>Car rental site application. Complete with backend and dashboard</p>
+              <p>Application developed during web and application courses at NTNU</p>
             </CardContent>
           </Card>
           <Card>
             <CardHeader>
-              <CardTitle>This is a card</CardTitle>
+              <CardTitle>Train Departure application</CardTitle>
             </CardHeader>
             <CardContent>
-              <p>This is some content inside this wonderfull card</p>
+              <p>Commandline interface for managing train departures</p>
+              <p>Application developed during first semester at NTNU</p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Chaos Game</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>JavaFX application for displaying mathematical fractals based on a list of transformations</p>
+              <p>Application developed during second semester at NTNU</p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Greenhouse networking application</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Server and clients for greenhouse management, focusing on network communication between users device and sensors in a greenhouse</p>
+              <p>Application developed during third semester at NTNU</p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Todolist application</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Todolist application with ability for creating multiple todolists</p>
+              <p>Application developed during fifth semester at NTNU</p>
             </CardContent>
           </Card>
         </div>
-      </div>
+      </div> */}
     </div>
 
   );
