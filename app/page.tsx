4a1ab4fa3e269ebe619fab9def8f1970c3ed0077
Stian <stianoyejenssen@gmail.com>
2026-01-25 18:17:38 +0100

    feat: :sparkles: Add projets Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,72 +1,61 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
+  const projects = [
+    {
+      title: "Portfolio",
+      description: "Website where I show off some of my work",
+      badges: ["React", "Next", "Shadcn", "Github Actions", "Github Pages"],
+    },
+    {
+      title: "Driveo",
+      description:
+        "Car rental site application. Complete with backend and dashboard. \nApplication developed during web and application courses at NTNU",
+      badges: ["Fullstack", "React", "Spring-Boot", "Postgres", "nginx", "Javascript", "Rest-Api", "JWT", "Axios"],
+    },
+    {
+      title: "Todolist application",
+      description: "Todolist application with ability for creating multiple todolists. \nApplication developed during fifth semester at NTNU",
+      badges: ["Flutter", "Android"],
+    },
+  ]
+
   return (
     <div>
-      <div className="flex min-h-screen flex-col items-center justify-center pt-4 bg-[linear-gradient(var(--accent)_1px,transparent_1px),linear-gradient(90deg,var(--accent)_1px,transparent_1px)] bg-[size:40px_40px] bg-[position:20px_20px] bg-fixed">
-        <main className="flex flex-col w-full items-center gap-4 h-min text-center">
-          <h1 className="text-4xl">Big Things Coming!</h1>
+      <div className="flex min-h-screen flex-col items-center justify-center pt-4 bg-[linear-gradient(var(--accent)_1px,transparent_1px),linear-gradient(90deg,var(--accent)_1px,transparent_1px)] bg-size-[40px_40px] bg-position-[20px_20px] bg-fixed">
+        <main className="flex flex-col items-center gap-4 h-min text-left">
+          <h1 className="text-8xl">
+            <span className="font-bold">Stian&apos;s</span> portfolio
+          </h1>
+          <h2 className="text-4xl">Big Things Coming!</h2>
           <p className="italic">Stay tuned</p>
         </main>
       </div>
-      {/* <div className="w-full flex flex-col items-center justify-center pt-4 pb-4 gap-4">
-        <h2 className="text-2xl">Projects</h2>
-        <div className="flex flex-col gap-4">
-          <Card>
-            <CardHeader>
-              <CardTitle>Portfolio</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>Website where i show off some of my work</p>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Driveo</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>Car rental site application. Complete with backend and dashboard</p>
-              <p>Application developed during web and application courses at NTNU</p>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Train Departure application</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>Commandline interface for managing train departures</p>
-              <p>Application developed during first semester at NTNU</p>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Chaos Game</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>JavaFX application for displaying mathematical fractals based on a list of transformations</p>
-              <p>Application developed during second semester at NTNU</p>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Greenhouse networking application</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>Server and clients for greenhouse management, focusing on network communication between users device and sensors in a greenhouse</p>
-              <p>Application developed during third semester at NTNU</p>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Todolist application</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>Todolist application with ability for creating multiple todolists</p>
-              <p>Application developed during fifth semester at NTNU</p>
-            </CardContent>
-          </Card>
-        </div>
-      </div> */}
+      <div className="w-full p-4 flex flex-col items-center gap-4">
+        <h2 className="text-3xl">Projects</h2>
+        <Carousel className="w-3xl max-w-screen p-10">
+          <CarouselPrevious />
+          <CarouselContent>
+            {projects.map((project, index) => (
+              <CarouselItem key={index}>
+                <h1 className="text-2xl mb-4">{project.title}</h1>
+                <div className="flex flex-col gap-4">
+                  <div className="flex flex-wrap gap-2">
+                    {project.badges.map((badge, index) => (
+                      <Badge key={index}>{badge}</Badge>
+                    ))}
+                  </div>
+                  <Separator />
+                  <p>{project.description}</p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
 
   );
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -8,7 +8,7 @@ import { ModeToggle } from "../theme-toggle"
 
 export default function Header() {
     return <div className="flex justify-center absolute w-full">
-        <div className="flex justify-between m-4 w-full bg-background p-2 border rounded-2xl md:max-w-2xl">
+        <div className="flex justify-between m-4 w-full bg-background p-2 border rounded-2xl max-w-4xl">
             <NavigationMenu>
                 <NavigationMenuList>
                     <NavigationMenuItem>
